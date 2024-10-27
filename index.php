9d93d514a3fa443c10a5d51c35c4240587b0cdea
Pietro Battistini <161157164+bqit@users.noreply.github.com>
2024-10-27 22:21:07 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Login Page: <a href="./php/login.php">Here</a></p>
+    <p>Sign up Page: <a href="./php/signup.php">Here</a></p>
+</body>
+</html>
